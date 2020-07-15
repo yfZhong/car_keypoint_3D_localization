f1fc93ff93ebf12f3ce6b2b7459a2da2e29acb03
yfzhong <yfzhong@aibee.com>
2020-07-16 00:04:04 +0800

    modify 'ROOT_FOLD' in scripts/run_hdfs_data_pred.sh

--- scripts/run_hdfs_data_pred.sh
+++ scripts/run_hdfs_data_pred.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# root fold
+CURR_FOLD="$(dirname "$(readlink -f "$0")")"
+ROOT_FOLD=${CURR_FOLD}/../
+
 if [ $# -lt 3 ];
 then
     echo "Illegal number of parameters!"
@@ -7,7 +11,7 @@ then
 fi
 
 #set env
-. ./set_env.sh
+. ${CURR_FOLD}/set_env.sh
 
 if [ -f "program_id.txt" ]
 then
@@ -17,9 +21,10 @@ fi
 echo $1 $2 $3 $4 >"program_id.txt"
 
 # download data
-bash download.sh
+bash ${CURR_FOLD}/download.sh
 
 # run pred
+cd ${ROOT_FOLD}
 python lib/pred_imgs.py \
     --config-file-box ../maskrcnn-benchmark/configs/caffe2/e2e_faster_rcnn_R_101_FPN_1x_caffe2_4s_shop.yaml \
     --config-file-kp ../maskrcnn-benchmark/configs/caffe2/e2e_keypoint_only_rcnn_R_101_FPN_1x_caffe2_4s_shop_pad.yaml \
@@ -30,5 +35,5 @@ python lib/pred_imgs.py \
     --vis_preds True
 
 # upload result
-bash upload.sh
+bash ${CURR_FOLD}/upload.sh
 
